282be12a6a6ddd921552dc18617beadf74f4f9b6
Julie Ng <me@julie.io>
2020-05-24 12:11:19 +0200

    compile-sass: fix source path, adjust writes, move outside assets/
    - the `styles.css` file will be overwritten
    - the versioned file, e.g. `styles-v1.4` will not be overwritten
    - compile-sass.js moved outside assets/ folder so Azure CLI does not copy to Storage Account
      the `--pattern` flag is too limited to exclude the file.

---
+++ compile-sass.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const sass = require('node-sass')
+
+const appVersion = process.env.npm_package_version
+
+const sourceFile = path.join(__dirname, 'assets/css/styles.scss')
+const outFile = path.join(__dirname, `assets/css/styles.css`)
+const outFileVersioned = path.join(__dirname, `assets/css/styles-${appVersion}.css`)
+const opts = {
+	file: sourceFile,
+	outputStyle: 'compressed'
+}
+
+sass.render(opts, function (err, result) {
+	if (result) {
+		const css = result.css.toString()
+		console.log('[Compiled CSS]')
+		console.log(css)
+
+		// Always write styles.css
+		fs.writeFileSync(outFile, css)
+		console.log('[Created] styles.css')
+
+		// Only write versioned file if doesn't exist
+		try {
+			fs.writeFileSync(outFileVersioned, css, { flag: 'wx' })
+			console.log(`[Created] styles-${appVersion}.css`)
+		} catch (e) {
+			console.log(`[Skipped] styles-${appVersion}.css already exists`)
+		}
+		process.exit(0)
+	}
+
+	if (err) {
+		console.error('Error: could not compile sass:')
+		console.log(err)
+	}
+
+	process.exit(1)
+})
