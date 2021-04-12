14cac41314e425c9e3963f872092f98011ca2e72
Julie Ng <me@julie.io>
2021-04-12 14:38:12 +0200

    fix(app-insights): only load and setup once

--- app/middleware/monitor.js
+++ app/middleware/monitor.js
@@ -3,9 +3,14 @@
 const appInsights = require('applicationinsights')
 const appInsightsKey = process.env.APPINSIGHTS_INSTRUMENTATION_KEY || false
 
+if (appInsightsKey) {
+	appInsights
+		.setup(appInsightsKey)
+		.start()
+}
+
 function middleware (req, res, next) {
 	if (appInsightsKey) {
-		appInsights.setup(appInsightsKey).start()
 		appInsights.defaultClient.trackNodeHttpRequest({
 			request: req,
 			response: res
